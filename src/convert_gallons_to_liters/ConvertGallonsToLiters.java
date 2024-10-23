48dbf74e3e4f9f830493e5228632ac3025cb38a0
Yuliya Zimenina <yuliya.zimenina@gmail.com>
2024-10-23 14:29:59 +0200

    Learning JavaCore v.17

---
+++ src/convert_gallons_to_liters/ConvertGallonsToLiters.java
@@ -0,0 +1,12 @@
+package convert_gallons_to_liters;
+
+public class ConvertGallonsToLiters {
+    public static void main(String[] args) {
+        double gallons; // The variable stores the number of gallons
+        double liters; // The variable stores the result of converting gallons to liters
+
+        gallons = 10;
+        liters = gallons * 3.7854; // Convert gallons to liters
+        System.out.println(gallons + " gallons equals " + liters + " liters");
+    }
+}
