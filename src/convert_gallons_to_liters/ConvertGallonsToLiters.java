298153940d3af56157ecad59f5cdc27b62bfffbd
Yuliya Zimenina <yuliya.zimenina@gmail.com>
2024-10-23 14:39:48 +0200

    Change program (add operator "If" and cycle "For")

--- src/convert_gallons_to_liters/ConvertGallonsToLiters.java
+++ src/convert_gallons_to_liters/ConvertGallonsToLiters.java
@@ -4,9 +4,17 @@ public class ConvertGallonsToLiters {
     public static void main(String[] args) {
         double gallons; // The variable stores the number of gallons
         double liters; // The variable stores the result of converting gallons to liters
+        int counter = 0;
 
-        gallons = 10;
-        liters = gallons * 3.7854; // Convert gallons to liters
-        System.out.println(gallons + " gallons equals " + liters + " liters");
+        for (gallons = 1; gallons <= 100; gallons ++) {
+            liters = gallons * 3.7854; // Convert gallons to liters
+            System.out.println(gallons + " gallons equals " + liters + " liters");
+            counter ++;
+            // After ever 10 lines we output an empty line
+            if(counter == 10){ // If the counter values is 10, we output an empty line
+                System.out.println();
+                counter = 0; // Reset counter
+            }
+        }
     }
 }
