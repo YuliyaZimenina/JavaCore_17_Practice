e53223156beb922562a18070c0a07caf16dcbed3
Yuliya Zimenina <yuliya.zimenina@gmail.com>
2024-10-24 13:29:47 +0200

    Add game "Guess letter"

---
+++ src/game_guess_the_letter/GameGuess.java
@@ -0,0 +1,12 @@
+package game_guess_the_letter;
+
+public class GameGuess {
+    public static void main(String[] args) throws java.io.IOException{
+        char letter, answer = 'J';
+
+        System.out.println("The hidden letter between A and Z");
+        System.out.println("Try to guess the letter ");
+        letter = (char) System.in.read(); // Reading a character from the keyboard
+        if (letter == answer) System.out.println("*** RIGHT ***");
+    }
+}
