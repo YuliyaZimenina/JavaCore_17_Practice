ced55b0bd354c995673dd6b0835180add4a5e1db
Yuliya Zimenina <yuliya.zimenina@gmail.com>
2024-10-25 12:24:39 +0200

    Add game "Guess letter" Add operator "else" Added nested operator if-else Added cycle do-while

--- src/game_guess_the_letter/GameGuess.java
+++ src/game_guess_the_letter/GameGuess.java
@@ -2,17 +2,24 @@ package game_guess_the_letter;
 
 public class GameGuess {
     public static void main(String[] args) throws java.io.IOException{
-        char letter, answer = 'J';
+        char letter, ignore, answer = 'J';
 
-        System.out.println("The hidden letter between A and Z");
-        System.out.println("Try to guess the letter ");
-        letter = (char) System.in.read(); // Reading a character from the keyboard
-        if (letter == answer) System.out.println("*** RIGHT ***");
-        else{
-            System.out.println("*** WRONG ***");
-            if (letter < answer) System.out.println("The hidden letter is located further down" +
-                    " the alphabet");
-            else System.out.println("The hidden letter is located closer in the alphabet");
-        }
+        do {
+            System.out.println("The hidden letter between A and Z");
+            System.out.println("Try to guess the letter ");
+            letter = (char) System.in.read(); // Reading a character from the keyboard
+            //Remove all remaining characters from the input buffer
+            do{
+                ignore = (char) System.in.read();
+            }while (ignore != '\n');
+            if (letter == answer) System.out.println("*** RIGHT ***");
+            else {
+                System.out.println("*** WRONG ***");
+                if (letter < answer) System.out.println("The hidden letter is located further down" +
+                        " the alphabet");
+                else System.out.println("The hidden letter is located closer in the alphabet");
+                System.out.println("Try again! \n");
+            }
+        }while (answer != letter);
     }
 }
